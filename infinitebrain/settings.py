27be277d019c0fe28893944f0351541ba4dd1d81
Michael Romano <michaelfromano@gmail.com>
2014-10-22 14:32:11 -0400

    Changed db

--- infinitebrain/settings.py
+++ infinitebrain/settings.py
@@ -119,14 +119,14 @@ SOUTH_MIGRATION_MODULES = {
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = { 'default': {
-        # 'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        # 'NAME': 'modulator',                  # Or path to database file if using sqlite3.
-        # 'USER': 'dev',                      # Not used with sqlite3.
-        # 'PASSWORD': 'l3rAInzRck',                  # Not used with sqlite3.
-        'ENGINE': 'django.db.backends.sqlite3',  # this just added for development
-        'NAME': 'devdb',  # again, just added for development
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'modulator',                  # Or path to database file if using sqlite3.
+        'USER': 'dev',                      # Not used with sqlite3.
+        'PASSWORD': 'l3rAInzRck',                  # Not used with sqlite3.
+        # 'ENGINE': 'django.db.backends.sqlite3',  # this just added for development
+        # 'NAME': 'devdb',  # again, just added for development
+        # 'USER': '',
+        # 'PASSWORD': '',
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
