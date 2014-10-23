77a2257bf1f5a0613b27722c092fddd6b951b61d
Michael Romano <michaelfromano@gmail.com>
2014-10-22 21:26:01 -0400

    in modeldb, added management/commands/populate.py

---
+++ modeldb/management/commands/populate.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from modeldb.models import Model, ModelSpec, ModelRelation
+
+
+# Fields for Model are as follows:
+'''
+Model(models.Model)
+    user = models.ForeignKey(User, blank=True, null=True, related_name='user_model')
+    name=models.CharField(max_length=1000)
+    level=models.CharField(max_length=10, default='network') # options: {mechanism, node, network}
+    notes=models.CharField(max_length=10000, default='')
+    privacy=models.CharField(max_length=10, default='unlisted') # options: {private, public, unlisted}
+    rating = models.IntegerField(default=0) 
+    d3file = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='models',null=True)
+    readmefile = models.FileField(storage=FileSystemStorage(location=settings.MEDIA_ROOT), upload_to='models',null=True)
+    date_added =models.DateTimeField(default=datetime.now, null=True)
+    tags = TaggableManager()
+    votes = VotesField()
+    objects = models.Manager() # Add objects before all other managers to avoid issues mention in http://stackoverflow.com/a/4455374/1462141
+    objects_with_scores = ObjectsWithScoresManager() #For just a list of objects that are not ordered that can be customized.
+    sort_by_score
+'''
+class Command(BaseCommand):
+    args = ''
+    help = ''
+
+    def _create_model(self):
+        model = Model(
+            name='Mike',
+            level='network',
+            notes='practice_populate',
+            privacy='public',
+            )
+        model.save()
+
+    def handle(self, *args, **kwargs):
+        self._create_model()
